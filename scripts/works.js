235e7b79002a60b480e72eeec7fdb792f2765eb3
erick almeida <erick.almeida86@hotmail.com>
2024-07-24 15:17:56 -0300

    final: website ready for launch

--- scripts/works.js
+++ scripts/works.js
@@ -1,15 +1,17 @@
 const works = [
   {
-    path: "",
-    imageUrl: "",
-    title: "",
-    occupation: "",
+    path: "https://euerickap.github.io/devlinks/",
+    imageUrl: "assets/devlinks.webp",
+    imageAlt: "Imagem do projeto DevLinks",
+    date: "07/2023",
+    title: "DevLinks",
   },
   {
-    path: "",
-    imageUrl: "",
-    title: "",
-    occupation: "",
+    path: "https://euerickap.github.io/doctorcare/",
+    imageUrl: "assets/doctorcare.webp",
+    imageAlt: "Imagem do projeto DoctorCare",
+    date: "07/2022",
+    title: "DoctorCare",
   },
 ];
 
@@ -18,15 +20,24 @@ const worksList = document.querySelector(".works");
 works.forEach((item) => {
   const li = document.createElement("li");
   const a = document.createElement("a");
+  const div = document.createElement("div");
   const img = document.createElement("img");
-  const h3 = document.createElement("h3");
+  const h4 = document.createElement("h4");
   const span = document.createElement("span");
 
   a.href = item.path;
 
-  a.appendChild(span);
-  a.appendChild(h3);
-  a.appendChild(img);
-  li.appendChild(a);
+  img.src = item.imageUrl;
+  img.alt = item.imageAlt;
+
+  span.textContent = item.date;
+
+  h4.textContent = item.title;
+
   worksList.appendChild(li);
+  li.appendChild(a);
+  a.appendChild(div);
+  div.appendChild(img);
+  a.appendChild(span);
+  a.appendChild(h4);
 });
