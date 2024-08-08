63cd368b9162a28d271d3d771fe26deefe3e35cf
erick almeida <erick.almeida86@hotmail.com>
2024-08-08 01:19:26 -0300

    fixed: biography

--- scripts/biography.js
+++ scripts/biography.js
@@ -1,7 +1,7 @@
 const paragraphs = [
   "Muito prazer, me chamo Erick e durante minha jornada, tive a oportunidade de conhecer a programação, onde me encontrei profissionalmente.",
-  "Estudante de desenvolvimento Front-End, tenho focado na área através de estudos particulares através de cursos livres, bootcamps, eventos e ainda sonho em entrar para uma faculdade.",
-  "Busco oportunidade para atuar com desenvolvimento Front-End, seja em estágio ou trabalho volutário. Se você visitou meu site e tem interesse para uma oportunidade, entre em contato por aqui.",
+  "Estudante de desenvolvimento Front-End, tenho focado na área através de estudos particulares, de cursos livres, bootcamps, eventos e ainda sonho em entrar para uma faculdade.",
+  "Busco oportunidade para atuar com desenvolvimento Front-End, seja em estágio ou trabalho voluntário. Se você visitou meu site e tem interesse para uma oportunidade, entre em contato por aqui.",
 ];
 
 const paragraphList = document.querySelector(".biography");
